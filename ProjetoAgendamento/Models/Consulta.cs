6ed758b91f92be5d6a223b3966ac4aad476f40d6
Rafael Carcuchinski Vargas <rafaelcarcuchinski@gmail.com>
2015-10-13 22:33:53 -0300

    Adicionada consulta.cs

---
+++ ProjetoAgendamento/Models/Consulta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAgendamento.Models
+{
+    public class Consulta
+    {
+        [Key]
+        public int IdConsulta { get; set; }
+        public int idPaciente { get; set; }
+        public int idEspecialidade { get; set; }
+        public int idMedico { get; set; }
+        public string dataAgendamento { get; set; }
+        public string dataConsulta { get; set; }
+        public string horarioConsulta { get; set; }
+    }
+    
+}
