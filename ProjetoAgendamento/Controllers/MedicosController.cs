1704581040642204df55c203a0fe754c09a03211
fhelwanger <fhelwanger@gmail.com>
2015-11-22 16:04:39 -0200

    SPRINT D - TAREFA 19.1.1

--- ProjetoAgendamento/Controllers/MedicosController.cs
+++ ProjetoAgendamento/Controllers/MedicosController.cs
@@ -7,7 +7,7 @@ using ProjetoAgendamento.Models;
 
 namespace ProjetoAgendamento.Controllers
 {
-    [Authorize(Roles="Secretaria")]
+    [Authorize(Roles = "Secretaria")]
     public class MedicosController : Controller
     {
         public ActionResult Index(string msg)
@@ -60,6 +60,50 @@ namespace ProjetoAgendamento.Controllers
             ctx.SaveChanges();
 
             return RedirectToAction("Index", new { msg = "Médico cadastrado com sucesso" });
+        }
+
+        public ActionResult Editar(int id)
+        {
+            var ctx = new ApplicationDbContext();
+            var medico = ctx.Medicos.FirstOrDefault(m => m.IdMedico == id);
+
+            if (medico == null || medico.Ativo == false)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(medico);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Medico medico)
+        {
+            var ctx = new ApplicationDbContext();
+
+            if (ctx.Medicos.FirstOrDefault(m => m.CRM == medico.CRM && m.IdMedico != medico.IdMedico) != null)
+            {
+                ModelState.AddModelError("CRM", "Já existe um médico cadastrado com este CRM");
+            }
+            
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var medicoExistente = ctx.Medicos.Find(medico.IdMedico);
+
+            medicoExistente.CRM = medico.CRM;
+            medicoExistente.Nome = medico.Nome;
+            medicoExistente.idEspecialidade = medico.idEspecialidade;
+            medicoExistente.DataNascimento = medico.DataNascimento;
+            medicoExistente.SexoMasculino = medico.SexoMasculino;
+            medicoExistente.Telefone = medico.Telefone;
+            medicoExistente.Email = medico.Email;
+            medicoExistente.Observacoes = medico.Observacoes;
+
+            ctx.SaveChanges();
+
+            return RedirectToAction("Index", new { msg = "Médico alterado com sucesso" });
         }
     }
 }
