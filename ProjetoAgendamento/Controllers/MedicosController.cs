1015988514a7acbb0b2a37b33cd1e83ab441b2f4
fhelwanger <fhelwanger@gmail.com>
2015-11-22 12:42:31 -0200

    SPRINT D - TAREFA 15.1.1

---
+++ ProjetoAgendamento/Controllers/MedicosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoAgendamento.Models;
+
+namespace ProjetoAgendamento.Controllers
+{
+    [Authorize(Roles="Secretaria")]
+    public class MedicosController : Controller
+    {
+        public ActionResult Index()
+        {
+            var ctx = new ApplicationDbContext();
+            var lista = ctx.Medicos
+                .Include("Especialidade")
+                .OrderBy(m => m.Nome);
+
+            return View(lista);
+        }
+    }
+}
